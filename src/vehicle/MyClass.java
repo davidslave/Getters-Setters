b333097c17c0c0cb2a71e41c2c91c7c1731ec415
davidslave <slavevskidavid@gmail.com>
2023-12-01 10:01:53 +0100

    add Getters & Setters

---
+++ src/vehicle/MyClass.java
@@ -0,0 +1,20 @@
+package vehicle;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+	Vehicle v1 = new Vehicle();
+	v1.setColor("red");
+	System.out.println(v1.getColor());;
+Vehicle v2 = new Vehicle();
+v1.setX(10);
+System.out.println(v1.getX());
+Vehicle v3 = new Vehicle();
+v1.setNew("New");
+System.out.println(v1.getNew());
+Vehicle v4 = new Vehicle();
+v1.setY(9);
+System.out.println(v1.getY());
+	}
+}
+
---
+++ src/vehicle/Vehicle.java
@@ -0,0 +1,40 @@
+package vehicle;
+
+public class Vehicle {
+private String color;
+private int x;
+private String New;
+private int y;
+public int getY() {
+	return y;
+}
+
+public void setY(int y) {
+	this.y = y;
+}
+
+public String getNew() {
+	return New;
+}
+
+public void setNew(String new1) {
+	New = new1;
+}
+
+public int getX() {
+	return x;
+}
+
+public void setX(int x) {
+	this.x = x;
+}
+
+public String getColor() {
+	return color;
+}
+
+public void setColor(String color) {
+	this.color = color;
+}
+
+}
